4d741aaf8a6235868c77384edac0a0417dfef350
ntmhrt <nhirota@berklee.edu>
2026-02-08 11:27:36 -0500

    add files

---
+++ Week3/icecreamshop1.js
@@ -0,0 +1,9 @@
+const priceOfIceCream = 3;
+let paymentRecieved = Number(prompt("How much?"));
+let isPaymentEnough = paymentRecieved >= priceOfIceCream;
+if (isPaymentEnough){
+    print("Thank you!")
+}else{ (isPaymentEnough) 
+    print("it's $3.")
+}
+
---
+++ Week3/icecreamshop2.js
@@ -0,0 +1,3 @@
+const conesSoldPerHour = 14;
+for (let hour = 1; hour <= 12; hour ++)
+print(conesSoldPerHour * hour + " sold at hour " + hour );
